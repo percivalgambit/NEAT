fc1469aef305521e77fbe6591b4cf09d3a7c04ac
Lee Ehudin <ehudinl@uchicago.edu>
2014-12-13 17:37:20 -0500

    Wrrote better tests

--- sse_sample_app.c
+++ sse_sample_app.c
@@ -1,10 +1,8 @@
 
 /*! @file
- *  This is a sample application using SSE floating-point arithmetic instructions to
+ * This is a sample application using SSE floating-point arithmetic instructions to
  * test the ftrace tool.
  */
-
-#include <xmmintrin.h>
 
 /*!
  * The main procedure of the application.
@@ -12,15 +10,25 @@
  * @param[in]   argv            array of command line arguments
  */
 int main(int argc, char *argv[]) {
-    float a_double[] __attribute__ ((aligned (16))) = { 2.0,  0, 0, 2.0 };
-    float b_double[] __attribute__ ((aligned (16))) = { 0.5,  0, 0, 0.5 };
+    // Make sure that normal arithmetic operations produce the right output
+    float a = 2.0f;        //!< a = 2.0
+    float b = 0.3f;        //!< b = 0.3
+    float c = a + b;       //!< c = 2.0 + 0.3 = 2.3
+    float d = b - a;       //!< d = 0.3 - 2.0 = -1.7
+    float e = a * c;       //!< e = 2.0 * 2.3 = 4.6
+    float f = a / b;       //!< f = 2.0 / 0.3 = 6.666666...
+    float g = (f + a) / b; //!< g = (6.66666... + 2.0) / 0.3 = 28.888885
 
-    __m128 *a_ptr = (__m128 *)a_double;
-    __m128 *b_ptr = (__m128 *)b_double;
+    // Make sure that arithmetic instructions operating on two registers work correctly
+    float h = b + b;       //!< h = 0.3 + 0.3 = 0.6
 
-    __m128 c = _mm_add_ss(*a_ptr, *b_ptr);
-    __m128 d = _mm_mul_ss(c, *b_ptr);
-    __m128 e = _mm_sub_ss(*a_ptr, *b_ptr);
-    __m128 f = _mm_div_ss(*a_ptr, e);
+    // Make sure that arithmetic with large and small numbers produces the right output
+    float i = 6.01e+30f;   //!< i = 6.01e+30
+    float j = i + a;       //!< j = 6.01e+30 + 2.0 = 6.01e+30
+    float k = i * a;       //!< k = 6.01e+30 * 2.0 = 1.202e+31
+    float l = 1.2345e-35f; //!< l = 1.2345e-35
+    float m = l + a;       //!< m = 1.2345e-35 + 2.0 = 2.0
+    float n = l + l;       //!< n  = 1.2345e-35 + 1.2345e-35 = 2.4690e-35
+
     return 0;
 }
