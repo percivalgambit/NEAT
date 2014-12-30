1d87b2399f3e02ac9420d034fff7241f939ac4b5
Lee Ehudin <percivalgambit@gmail.com>
2014-12-30 00:18:25 -0500

    Documentation generated on 12/30/14 12:18am

---
+++ navtreedata.js
@@ -0,0 +1,21 @@
+var NAVTREE =
+[
+  [ "ftrace", "index.html", [
+    [ "Replacing floating-point instructions", "md_REPLACE_FP_CODE.html", null ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"files.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
